845e415133c2e760acd526f5535b721e08381a62
Yanggoing <32742330+Yanggoing@users.noreply.github.com>
2020-09-24 14:40:03 +0800

    --init--

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justify-content-between mb-4">
+    <a href="#" class="navbar-brand">者也专栏</a>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface UserProps {
+  isLogin: boolean;
+  name?: string;
+  id?: number;
+}
+
+export default defineComponent({
+  name: 'GlobalHeader',
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true
+    }
+  }
+})
+</script>
