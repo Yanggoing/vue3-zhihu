8341de621d9e66e0e696cc031d17afe382dc9fbd
Yanggoing <32742330+Yanggoing@users.noreply.github.com>
2020-09-24 21:04:14 +0800

    Validator

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="container">
-    <column-list :list="list"></column-list>
+    <global-header :user="currentUser"></global-header>
+    <form action="">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <input
+          type="text" class="form-control" id="exampleInputEmail1"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        >
+        <div class="form-text" v-if="emailRef.error">{{ emailRef.message }}</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+    </form>
+    <!-- <column-list :list="list"></column-list> -->
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
 
+const currentUser: UserProps = {
+  isLogin: true,
+  name: 'zy'
+}
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -24,14 +45,33 @@ const testData: ColumnProps[] = [
   }
 ]
 
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList
+    // ColumnList,
+    GlobalHeader
   },
   setup () {
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
+    const validateEmail = () => {
+      if (emailRef.val.trim() === '') {
+        emailRef.error = true
+        emailRef.message = 'can not be empty'
+      } else if (!emailReg.test(emailRef.val)) {
+        emailRef.error = true
+        emailRef.message = 'should be valid message'
+      }
+    }
     return {
-      list: testData
+      list: testData,
+      currentUser,
+      emailRef,
+      validateEmail
     }
   }
 })
