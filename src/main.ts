546fd757ff6974998d6a33c3aac3ddedb75a42b7
Yanggoing <32742330+Yanggoing@users.noreply.github.com>
2020-10-05 21:55:04 +0800

    --main--

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,18 @@ axios.defaults.baseURL = 'http://apis.imooc.com/api/'
 axios.interceptors.request.use(config => {
   config.params = { ...config.params, icode: 'A5B9533CCB47A692' }
   store.commit('setLoading', true)
+  store.commit('setError', { status: false, message: '' })
   return config
 })
 axios.interceptors.response.use(config => {
   setTimeout(() => {
     store.commit('setLoading', false)
-  }, 2000)
+  }, 100)
   return config
+}, err => {
+  const { error } = err.response.data
+  store.commit('setError', { status: true, message: error })
+  store.commit('setLoading', false)
 })
 
 const app = createApp(App)
