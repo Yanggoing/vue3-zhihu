740ac662f75a95a708c631db3cc283e5b68a1238
Yanggoing <32742330+Yanggoing@users.noreply.github.com>
2020-10-05 21:54:50 +0800

    --home--

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,32 +17,24 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import ColumnList, { ColumnProps } from '../components/ColumnList.vue'
+import { defineComponent, computed, onMounted } from 'vue'
+import ColumnList from '../components/ColumnList.vue'
+import { useStore } from 'vuex'
+import { GlobalDataProps } from '../store'
 
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧'
-    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
-]
 export default defineComponent({
   name: 'Home',
   components: {
     ColumnList
   },
   setup () {
+    const store = useStore<GlobalDataProps>()
+    onMounted(() => {
+      store.dispatch('fetchColumns')
+    })
+    const list = computed(() => store.state.columns)
     return {
-      list: testData
+      list
     }
   }
 })
