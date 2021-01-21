082adcca4be07f1f98383fa31081292e737b7642
zhaoyang <zhaoyang@agora.io>
2021-01-22 00:11:57 +0800

    Feat: post detail

--- src/views/PostDetail.vue
+++ src/views/PostDetail.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="post-detail-page">
-    <modal title="删除文章" :visible="modalIsVisible"
+    <modal
+      title="删除文章"
+      :visible="modalIsVisible"
       @modal-on-close="modalIsVisible = false"
       @modal-on-confirm="hideAndDelete"
     >
@@ -8,12 +10,12 @@
     </modal>
     <article class="w-75 mx-auto mb-5 pb-3" v-if="currentPost">
       <img :src="currentImageUrl" alt="currentPost.title" class="rounded-lg img-fluid my-4" v-if="currentImageUrl">
-      <h2 class="mb-4">{{currentPost.title}}</h2>
+      <h2 class="mb-4">{{ currentPost.title }}</h2>
       <div class="user-profile-component border-top border-bottom py-3 mb-5 align-items-center row g-0">
         <div class="col">
           <user-profile :user="currentPost.author" v-if="typeof currentPost.author === 'object'"></user-profile>
         </div>
-        <span class="text-muted col text-right font-italic">发表于：{{currentPost.createdAt}}</span>
+        <span class="text-muted col text-right font-italic">发表于：{{ currentPost.createdAt }}</span>
       </div>
       <div v-html="currentHTML"></div>
       <div v-if="showEditArea" class="btn-group mt-5">
@@ -35,7 +37,7 @@ import { defineComponent, onMounted, computed, ref } from 'vue'
 import MarkdownIt from 'markdown-it'
 import { useStore } from 'vuex'
 import { useRoute, useRouter } from 'vue-router'
-import { GlobalDataProps, PostProps, ImageProps, UserProps, ResponseType } from '../store'
+import { GlobalDataProps, PostProps, ImageProps, UserProps, ResponseType } from '@/store'
 import UserProfile from '../components/UserProfile.vue'
 import Modal from '../components/Modal.vue'
 import createMessage from '../components/createMessage'
@@ -58,13 +60,19 @@ export default defineComponent({
     })
     const currentPost = computed<PostProps>(() => store.getters.getCurrentPost(currentId))
     const currentHTML = computed(() => {
-      const { content, isHTML } = currentPost.value
+      const {
+        content,
+        isHTML
+      } = currentPost.value
       if (currentPost.value && content) {
         return isHTML ? content : md.render(content)
       }
     })
     const showEditArea = computed(() => {
-      const { isLogin, _id } = store.state.user
+      const {
+        isLogin,
+        _id
+      } = store.state.user
       if (currentPost.value && currentPost.value.author && isLogin) {
         const postAuthor = currentPost.value.author as UserProps
         return postAuthor._id === _id
@@ -85,7 +93,10 @@ export default defineComponent({
       store.dispatch('deletePost', currentId).then((rawData: ResponseType<PostProps>) => {
         createMessage('删除成功，2秒后跳转到专栏首页', 'success', 2000)
         setTimeout(() => {
-          router.push({ name: 'column', params: { id: rawData.data.column } })
+          router.push({
+            name: 'column',
+            params: { id: rawData.data.column }
+          })
         }, 2000)
       })
     }
