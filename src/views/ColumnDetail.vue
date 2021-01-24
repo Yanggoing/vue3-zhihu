9f061e00a028c82fd045948542e232691d8e4390
virtuosyo <virtuosyo@gmail.com>
2021-01-24 23:57:03 +0800

    Chore: css style

--- src/views/ColumnDetail.vue
+++ src/views/ColumnDetail.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="column-detail-page w-75 mx-auto">
-    <div class="column-info row mb-4 border-bottom pb-4 align-items-center" v-if="column">
+    <div
+      class="column-info row mb-4 border-bottom pb-4 align-items-center"
+      v-if="column"
+    >
       <div class="col-3 text-center">
-        <img :src="column.avatar && column.avatar.fitUrl" :alt="column.title" class="rounded-circle border w-100">
+        <img
+          :src="column.avatar && column.avatar.fitUrl"
+          :alt="column.title"
+          class="rounded-circle border w-100"
+        />
       </div>
       <div class="col-9">
         <h4>{{ column.title }}</h4>
@@ -14,12 +21,12 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, onMounted } from 'vue'
+import { computed, defineComponent, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useStore } from 'vuex'
-import { GlobalDataProps, ColumnProps } from '@/store'
+import { ColumnProps, GlobalDataProps } from '../store'
 import PostList from '../components/PostList.vue'
-import { addColumnAvatar } from '@/helper'
+import { generateFitUrl } from '../helper'
 
 export default defineComponent({
   components: {
@@ -34,18 +41,20 @@ export default defineComponent({
       store.dispatch('fetchPosts', currentId)
     })
     const column = computed(() => {
-      const selectColumn = store.getters.getColumnById(currentId) as ColumnProps | undefined
+      const selectColumn = store.getters.getColumnById(currentId) as ColumnProps
       if (selectColumn) {
-        addColumnAvatar(selectColumn, 100, 100)
+        generateFitUrl(selectColumn, 100, 100)
       }
       return selectColumn
     })
-    const list = computed(() => store.getters.getPostsByCid(currentId))
-
+    const list = computed(() => store.getters.getPostsById(currentId))
     return {
-      column,
-      list
+      list,
+      column
     }
   }
 })
 </script>
+
+<style>
+</style>
